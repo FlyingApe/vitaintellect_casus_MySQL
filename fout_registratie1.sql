c90bad34c3abf1ae09618c117a98005c7d6b7f1b
Bastiaan de Jong <bastiaan_sierd@hotmail.com>
2019-01-12 19:33:37 +0100

    Update fout_registratie1.sql

--- fout_registratie1.sql
+++ fout_registratie1.sql
@@ -1,16 +1,16 @@
 SELECT 	a.productieactiviteit as foutcode, 
-		c.activiteit,
+	c.activiteit,
         a.machinenummer as machine,
-		count(a.productieactiviteit) as "aantal fout", 
-		b.aantal as uit, 
-		round(((count(a.productieactiviteit)/b.aantal)*100), 1) as percentage
+	count(a.productieactiviteit) as "aantal fout", 
+	b.aantal as uit, 
+	round(((count(a.productieactiviteit)/b.aantal)*100), 1) as percentage
 FROM vitaintellectdb.productielogboek as a
 LEFT JOIN 
 (
 	SELECT productieactiviteit, count(productieactiviteit) as aantal
-    FROM vitaintellectdb.productielogboek
-    WHERE productieactiviteit < 90
-    GROUP BY productieactiviteit
+    	FROM vitaintellectdb.productielogboek
+    	WHERE productieactiviteit < 90
+    	GROUP BY productieactiviteit
 )as b on a.productieactiviteit=(b.productieactiviteit+90)
 LEFT JOIN vitaintellectdb.productieactiviteit as c ON a.productieactiviteit=c.activiteitnummer 
 WHERE a.productieactiviteit >= 90
